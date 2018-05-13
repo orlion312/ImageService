0125660124af25b76f46e5573807b311e24293a4
orlion312 <orlion312@gmail.com>
2018-05-13 11:58:46 +0300

    ImageServiceCommunication

---
+++ ImageServiceCommunication/IClientChannel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ImageServiceCommunication.Events;
+
+namespace ImageServiceCommunication
+{
+    interface IClientChannel
+    {
+        event EventHandler<DataReceivedEventArgs> DataReceived;
+        void start();
+        void close();
+    }
+}
---
+++ ImageServiceCommunication/TcpClientChannel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using ImageServiceCommunication.Events;
+
+namespace ImageServiceCommunication
+{
+    class TcpClientChannel : IClientCommunication
+    {
+        private int port;
+        private string ip;
+        private TcpClient client;
+        private IClientCommunication handler;
+
+        public TcpClientChannel(string ip, int port)
+        {
+            this.port = port;
+            this.ip = ip;
+            this.handler = null;
+        }
+
+        public event EventHandler<DataReceivedEventArgs> DataReceived;
+
+        public void close()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void HandleClient(TcpClient client)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int send(string data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void start()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
